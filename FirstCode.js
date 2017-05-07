0304137acd79629a9c836a60a4197284ead4caf6
rashmith222 <rashmith222@users.noreply.github.com>
2017-05-07 22:49:22 +0530

    Update FirstCode.js

--- FirstCode.js
+++ FirstCode.js
@@ -3,3 +3,7 @@ fs.readFile('Hello.txt', (err, data) => {
   if (err) console.log(err);
   console.log(data);
 });
+fs.readFile('Hello1.txt', (err, data) => {
+  if (err) console.log(err);
+  console.log(data);
+});
