2d152a788845a8fd6aac1bf0225c2d90ecec9d4c
rashmith222 <rashmith222@users.noreply.github.com>
2017-05-07 22:54:43 +0530

    Update FirstCode.js

--- FirstCode.js
+++ FirstCode.js
@@ -2,8 +2,8 @@ var fs = require('fs');
 fs.readFile('Hello.txt', (err, data) => {
   if (err) console.log(err);
   console.log(data);
-});
-fs.readFile('Hello1.txt', (err, data) => {
-  if (err) console.log(err);
-  console.log(data);
+  fs.readFile('Hello1.txt', (err, data) => {
+  	if (err) console.log(err);
+  	console.log(data);
+   });
 });
