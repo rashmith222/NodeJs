8ec93698f0e3c1a92475aa30f86a78c405a1c7a0
rashmith222 <rashmith222@users.noreply.github.com>
2017-05-08 19:39:39 +0530

    Update 03.js

--- 03.js
+++ 03.js
@@ -8,25 +8,18 @@ var myFirstFilePromise = new Promise(function(resolve, reject) {
     resolve(data);
   });
 });
-var mySecondFilePromise = new Promise(function(resolve, reject) {
-  fs.readFile('Hello1.txt', 'utf8', (err, data1) => {
-    if (err) {
-      reject(err);
-    }
-    resolve(data1);
-  });
-});
 myFirstFilePromise
+  .then(function(data) {
+    console.log(data);
+    return new Promise(function(resolve, reject) {
+      fs.readFile('Hello1.txt', 'utf8', (err, data1) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(data1);
+      });
+    });
+  })
   .then(function(data1) {
     console.log(data1);
-  })
-  .catch(function(err) {
-    console.error(err);
-  });
-mySecondFilePromise
-  .then(function(data1) {
-    console.log(data1);
-  })
-  .catch(function(err) {
-    console.error(err);
   });
