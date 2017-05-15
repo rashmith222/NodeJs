3929c31636fe39d29b06ce5f425a4ae4435b77be
rashmith222 <rashmith222@users.noreply.github.com>
2017-05-15 14:40:57 +0530

    Create 05.js
    04.js is for displaying two file contents using function and promises.

---
+++ 05.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+function readfilepromised(filename) {
+  return new Promise(function(resolve, reject) {
+    fs.readFile(filename, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(data);
+    });
+  });
+}
+readfilepromised('Hello.txt').then(function(data) {
+  console.log(data);
+});
+readfilepromised('Hello1.txt').then(function(data1) {
+  console.log(data1);
+});
