67a65e5c24b2774185f79a555f8b52c0de117b2c
rashmith222 <rashmith222@users.noreply.github.com>
2017-05-15 16:07:57 +0530

    Update 05.js

--- 05.js
+++ 05.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-
+var arr = ['Hello.txt', 'Hello1.txt', 'Hello2.txt'];
 function readfilepromised(filename) {
   return new Promise(function(resolve, reject) {
     fs.readFile(filename, 'utf8', (err, data) => {
@@ -10,12 +10,8 @@ function readfilepromised(filename) {
     });
   });
 }
-
-readfilepromised('Hello.txt')
-  .then(function(data) {
+for (var x = 0; x < arr.length; x++) {
+  readfilepromised(arr[x]).then(function(data) {
     console.log(data);
-    return readfilepromised('Hello1.txt');
-  })
-  .then(function(data1) {
-    console.log(data1);
   });
+}
