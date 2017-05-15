5a36cba79a5a4dcc4d45201e8d5fa61e8e234348
rashmith222 <rashmith222@users.noreply.github.com>
2017-05-15 14:44:19 +0530

    Update 03.js

--- 03.js
+++ 03.js
@@ -1,25 +1,17 @@
 var fs = require('fs');
-
-var myFirstFilePromise = new Promise(function(resolve, reject) {
-  fs.readFile('Hello.txt', 'utf8', (err, data) => {
-    if (err) {
-      reject(err);
-    }
-    resolve(data);
-  });
-});
-myFirstFilePromise
-  .then(function(data) {
-    console.log(data);
-    return new Promise(function(resolve, reject) {
-      fs.readFile('Hello1.txt', 'utf8', (err, data1) => {
-        if (err) {
-          reject(err);
-        }
-        resolve(data1);
-      });
+function readfilepromised(filename) {
+  return new Promise(function(resolve, reject) {
+    fs.readFile(filename, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(data);
     });
-  })
-  .then(function(data1) {
-    console.log(data1);
   });
+}
+readfilepromised('Hello.txt').then(function(data) {
+  console.log(data);
+});
+readfilepromised('Hello1.txt').then(function(data1) {
+  console.log(data1);
+});
